19677c6c7ea73c10d085ca6aac0fe9a7c73d61b1
Daisuke Kato <kato.daisuke429@gmail.com>
2019-04-15 10:41:42 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+extern crate ansi_term;
+
+use ansi_term::Colour;
+
+fn main() {
+    println!(
+        "This is {} in color, {} in color and {} in color",
+        Colour::Red.paint("red"),
+        Colour::Blue.paint("blue"),
+        Colour::Green.paint("green")
+    );
+}
